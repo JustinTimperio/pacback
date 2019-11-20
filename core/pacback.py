5f5ef81cac24eb6dca28b0855b0371c31136a679
Justin Timperio <justintimperio@gmail.com>
2019-11-20 14:39:35 -0500

    All restore point functions working.

--- core/pacback.py
+++ core/pacback.py
@@ -1,27 +1,29 @@
 #! /usr/bin/python
 #### A utility for marking and restoring stable arch packages
-## Version 0.3
+## Version 0.5
 from python_scripts import *
 import re, tqdm #, pyinquirer
 
 base_dir = os.path.dirname(os.path.realpath(__file__))[:-5]
-
 #######################
 ### Restore Points
 ###################
 
 def create_restore_point(rp_num, dir_list='nil'):
-    ### Get Current Pkg List and Search the File System
+    ### Get Current Pkg List and Scan the File System
     rp_path = base_dir + '/restore-points/rp' + str(rp_num)
     print('Retrieving Current Stable Packages...')
     os.system("pacman -Q | sed -e 's/ /-/g' > " + rp_path + '.inv')
     packages = read_list(rp_path + '.inv')
     fs_list = set(search_fs('/var/cache/pacman', 'set') | search_fs('~/.cache', 'set'))
 
-    ### Loop Over Files Searching for Pkgs
-    found_pkgs = set()
+    ### Each Pkg Name Needs to be Escaped (like c++)
+    packages = read_list(rp_path + '.inv')
     re_pkgs = list(re.escape(pkg) for pkg in packages)
     bulk_search = ('|'.join(re_pkgs))
+    
+    ### Loop Over Files Searching for Pkgs
+    found_pkgs = set()
     for f in tqdm.tqdm(fs_list, desc='Bulk Scanning for ' + str(len(packages)) + ' Packages'):
         if re.findall(bulk_search, f.lower()):
             found_pkgs.add(f)
@@ -65,27 +67,58 @@ def rollback_to_rp(rp_num):
         print('Unpacking Files from Restore Point Tar....')
         untar_dir(rp_path + '.tar')
     else:
-        print('Restore Point #' + rp_num + ' Was NOT FOUND!')
-        return
+        return print('Restore Point #' + rp_num + ' Was NOT FOUND!')
 
     ### Install Restore Point Packages
     os.system('sudo pacman -U ' + rp_path + '/pac_cache/* --needed')
     shutil.rmtree(rp_path + '/pac_cache')
-    print('Any Directories Added to the Restore Point are Now Availble in: ' + rp_path)
+    
+    ### Check If There Are Any Files to Diff
+    rp_fs = search_fs(rp_path)
+    if not len(rp_fs) > 0:
+        return print('Rollback to Restore Point #' + rp_num + ' Complete!')
+    diff_yn = yn_frame('Do You Want to Checksum Diff Restore Point Files Against Your Current File System?')
+    if diff_yn == False:
+        return print('Skipping Diff! Restored Files are Now Availble in: ' + rp_path)
+
+    ### Diff Files Unpacked From Restore Point
+    elif diff_yn == True:
+        import multiprocessing as mp
+        ### Checksum Restore Point Files
+        with mp.Pool(os.cpu_count()) as pool:
+            rp_checksum = set(tqdm.tqdm(pool.imap(checksum_file, rp_fs),
+                            total=len(rp_fs), desc='Checksumming Restore Point Files'))
+        ### Checksum Source Files
+        sf_fs = set(path[len(rp_path):] for path in rp_fs)
+        with mp.Pool(os.cpu_count()) as pool:
+            sf_checksum = set(tqdm.tqdm(pool.imap(checksum_file, sf_fs),
+                            total=len(sf_fs), desc='Checksumming Source Files'))
+        ### Diff Restore Point Files Against Source
+        rp_checksum = set(path[len(rp_path):] for path in rp_checksum)
+        diff = sf_checksum.difference(rp_checksum)
+        if len(diff) > 0:
+            print('The Following Files Have Changed:')
+            for f in diff:
+                print(f)
+        else: 
+            print('No Files Have Been Changed!')
 
 #######################
 ### Rollback to Date
 ###################
 
 def rollback_to_date(date):
+    ### Validate Date Fromat and Build New URL
     if not re.findall(r'([12]\d{3}/(0[1-9]|1[0-2])/(0[1-9]|[12]\d|3[01]))', date):
         sys.exit('Invalid Date! Date Must be YYYY/MM/DD')
     archive_url=('https://archive.archlinux.org/repos/' + date + '/$repo/os/$arch')
-
+    
+    if re.findall(r'(https://archive\.archlinux\.org/repos/[12]\d{3}/(0[1-9]|1[0-2])/(0[1-9]|[12]\d|3[01])/\$repo/os/\$arch)'):
+        uncomment_line()
 
 ###############
 
-def release_rollback():
+def unlock_rollback():
     uncomment_lines('/etc/pacman.conf')
     return
 
@@ -93,12 +126,10 @@ def release_rollback():
 ### Parse Args
 ###################
 
-
 ### Tests
-custom_list = ['/var/app', '~/.ssh']
-create_restore_point(1, custom_list)
-#  create_restore_point(1)
+#  custom_list = ['~/.ssh']
+#  create_restore_point(2, custom_list)
+#  create_restore_point(2)
 #  rollback_to_date('1990/12/12')
-rollback_to_rp(1)
-
+#  rollback_to_rp(2)
 
